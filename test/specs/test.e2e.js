b68dc4978e14137628704f99340385ae18e8c785
Siarhei Ramanishkin <serjiond@gmail.com>
2026-03-27 00:00:12 +0200

    add all testst for UC-1 w\o POM; remove auto-generated POM files

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,15 +1,51 @@
-import { expect } from '@wdio/globals'
-import LoginPage from '../pageobjects/login.page.js'
-import SecurePage from '../pageobjects/secure.page.js'
+/* TODO
+ * UC-1 Form Validation (Negative Testing):
+ *Attempt to login with empty Username and Password. Verify error: "Username is required".
+ * Attempt to login with Username only. Verify error: "Password is required".
+ * Login with standard_user, go to Checkout, and attempt to continue without filling the postal code. Verify error message.
+ *
+ *
+ * UC-2 Handling Latency (Wait Strategies):
+ * Login using performance_glitch_user (This user has a built-in delay).
+ * Ensure your framework handles the page load delay gracefully without hard-coded pause() or sleep()commands.
+ * Reset the App State via the Burger Menu.
+ * Logout.
+ */
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open()
+describe("UC-1 Form Validation (Negative Testing)", () => {
+  beforeEach(async () => {
+    await browser.url("/");
+  });
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!')
-        await expect(SecurePage.flashAlert).toBeExisting()
-        await expect(SecurePage.flashAlert).toHaveText(
-            expect.stringContaining('You logged into a secure area!'))
-    })
-})
+  it("should check page title", async () => {
+    await expect(browser).toHaveTitle("Swag Labs");
+  });
 
+  it('should not login with empty credentials and display error message"Username is required"', async () => {
+    await $("#login-button").click();
+    await expect($("h3[data-test='error']")).toHaveText([
+      expect.stringContaining("Username is required"),
+    ]);
+  });
+
+  it('should not login with only username and display error message "Password is required"', async () => {
+    await $("#user-name").setValue("standard_user");
+    await $("#login-button").click();
+    await expect($("h3[data-test='error']")).toHaveText([
+      expect.stringContaining("Password is required"),
+    ]);
+  });
+
+  it("should not continue checkout without postal code and display error message", async () => {
+    // Login with standard_user
+    await $("#user-name").setValue("standard_user");
+    await $("#password").setValue("secret_sauce");
+    await $("#login-button").click();
+    await $(".shopping_cart_link").click();
+    await $("#checkout").click();
+    await $("#first-name").setValue("John");
+    await $("#last-name").setValue("Doe");
+    await $("#continue").click();
+    await expect($(".error-message-container")).toBeDisplayed();
+  });
+});
